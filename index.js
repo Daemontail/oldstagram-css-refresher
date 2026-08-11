3f9f7e6fdda15e4222049bf2927d8c985c5065e5
Daemontail <gurtejs169@gmail.com>
2026-08-11 23:52:21 +0530

    Refactored to only use 2 event listeners

--- index.js
+++ index.js
@@ -32,20 +32,21 @@ const main=document.getElementById("main")
 
 window.addEventListener("load",function(){
     let content=""
-    for(let i=0;i<posts.length;i++){
+    for(let val of posts){
+        let  {name,username,location,avatar,post,comment,likes} = val;
         content+=`
         <section class="post">
             <div class="post-info">
-                <img class="profile-icon" src=${posts[i].avatar}>
+                <img class="profile-icon" src=${avatar}>
                 <div>
-                <p class="bold profile-details">${posts[i].name}</p>
-                <p class="location profile-details" >${posts[i].location}</p>
+                <p class="bold profile-details">${name}</p>
+                <p class="location profile-details" >${location}</p>
                 </div>
             </div>
-            <img class="post-image" id=post-${i} src=${posts[i].post}>
+            <img class="post-image" data-dblclick=${username} src=${post}>
             <div class="interactibles">
             <button class="btn-icon">                
-            <img class="icon" id="like-${i}" src="images/icon-heart.png">
+            <img class="icon" data-click=${username} src="images/icon-heart.png">
             </button>
             <button class="btn-icon">
             <img class="icon" src="images/icon-comment.png">
@@ -53,24 +54,30 @@ window.addEventListener("load",function(){
             <button class="btn-icon">
             <img class="icon" src="images/icon-dm.png">
             </button>
-            <p class="bold"> <span id="like-counter-${i}">${posts[i].likes}</span> likes</p>
-            <p><span class="bold username">${posts[i].username}</span>${posts[i].comment}</p>
+            <p class="bold"> <span id=${username}>${likes}</span> likes</p>
+            <p><span class="bold username">${username}</span>${comment}</p>
             </div>
         </section>
         `
     }
     main.innerHTML+=content
-
-    for(let i=0;i<posts.length;i++){
-        document.getElementById(`post-${i}`).addEventListener("dblclick",function(){
-            let likes=JSON.parse(document.getElementById(`like-counter-${i}`).textContent)
+    document.addEventListener('click',function(e){
+        if(e.target.dataset.click){
+            // console.log(e.target.dataset.click)
+            const val=e.target.dataset.click
+            let likes=JSON.parse(document.getElementById(val).textContent)
             likes++
-            document.getElementById(`like-counter-${i}`).textContent=JSON.stringify(likes)
-        })
-        document.getElementById(`like-${i}`).addEventListener("click",function(){
-            let likes=JSON.parse(document.getElementById(`like-counter-${i}`).textContent)
+            document.getElementById(val).textContent=JSON.stringify(likes)
+        }
+    })
+    document.addEventListener('dblclick',function(e){
+        if(e.target.dataset.dblclick){
+            // console.log(e.target.dataset.click)
+            const val=e.target.dataset.dblclick
+            let likes=JSON.parse(document.getElementById(val).textContent)
             likes++
-            document.getElementById(`like-counter-${i}`).textContent=JSON.stringify(likes)
-        })
-    }
+            document.getElementById(val).textContent=JSON.stringify(likes)
+        }
+    })
 })
+
